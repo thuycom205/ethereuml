8b052006f4e8fe3e2e6fde7b72b43296d90c17e5
thuycom205 <luuthuy205@gmail.com>
2023-07-01 18:51:20 +0700

    Update gpt_ulity.sql

--- gpt_ulity.sql
+++ gpt_ulity.sql
@@ -22,3 +22,11 @@ FROM ( SELECT wp_posts.ID as ID, wp_posts.post_content as content , wctag.meta_v
 
 
 SELECT DISTINCT meta_value FROM wp_postmeta  where meta_key='tag';
+//check the origin 
+SELECT wp_posts.ID as ID, wp_posts.post_content as content , wctag.meta_value as sum_id FROM wp_posts
+                                                                                                 join wp_postmeta wmla on wp_posts.ID = wmla.post_id
+                                                                                                 join wp_postmeta wctag on  wp_posts.ID = wctag.post_id
+WHERE post_type = 'gpt_ori' and wmla.meta_key = 'tag' and wmla.meta_value = 'test1'
+ORDER BY sum_id asc ;
+//delete
+Delete from wp_posts where ID in (7846,7847,7848);
